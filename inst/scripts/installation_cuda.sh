6980ae4bb99fba7080009987f38b957df7f36d55
FranckRICHARD01 <franckrichard033@gmail.com>
2025-02-20 02:58:25 +0100

    Better INSTALL file

--- inst/scripts/installation_cuda.sh
+++ inst/scripts/installation_cuda.sh
@@ -26,7 +26,7 @@ while true; do
             echo "Removing old NVIDIA drivers and libraries..."
             sudo apt-get purge -y nvidia* libnvidia*
             sudo apt-get remove -y nvidia-*
-            sudo rm -f /etc/apt/sources.list.d/cuda*
+            sudo rm -f /etc/apt/sources.list.d/*cuda*
             sudo apt-get autoremove -y && sudo apt-get autoclean -y
             sudo rm -rf /usr/local/cuda*
             echo "Old NVIDIA drivers and libraries have been removed."
@@ -53,17 +53,36 @@ sudo apt install linux-headers-$(uname -r)
 # install other import packages
 sudo apt install g++ freeglut3-dev build-essential libx11-dev libxmu-dev libxi-dev libglu1-mesa libglu1-mesa-dev 
 
-# NVidia repository
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/x86_64/cuda-ubuntu2404.pin
-sudo mv cuda-ubuntu2404.pin /etc/apt/preferences.d/cuda-repository-pin-600
-sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/x86_64/3bf863cc.pub
-sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/x86_64/ /"
-sudo apt-get update
+echo "Choose an option:"
+echo "1) install the default version of cuda on your distribution "
+echo "2) install the version 12.8 of cuda (do not use if you're not sure)"
 
-# installing CUDA
-sudo apt install nvidia-cuda-dev nvidia-cuda-toolkit libthrust-dev libcub-dev
+read -p "Your choice (1 or 2): " choice
+
+case $choice in
+    1)
+        # installing default version of CUDA
+        sudo apt install nvidia-cuda-dev nvidia-cuda-toolkit libthrust-dev libcub-dev
+        ;;
+    2)
+        # NVidia repository
+        wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/x86_64/cuda-ubuntu2404.pin
+        sudo mv cuda-ubuntu2404.pin /etc/apt/preferences.d/cuda-repository-pin-600
+        sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/x86_64/3bf863cc.pub
+        sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/x86_64/ /"
+        sudo apt-get update
+
+        # installing CUDA
+        sudo apt install cuda-12-8 nvidia-driver-570 libthrust-dev libcub-dev
+        ;;
+    *)
+        echo "Invalid choice. Please enter 1 or 2."
+        ;;
+esac
+
 
 # setup your paths
+echo ''
 echo 'Please modify your .bashrc file located at the root of your account by adding the lines :'
 echo '  export PATH=/usr/local/cuda-xx.x/bin:$PATH'
 echo '  export LD_LIBRARY_PATH=/usr/local/cuda-xx.x/lib64:$LD_LIBRARY_PATH'
