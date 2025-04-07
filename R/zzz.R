22210a97c044bdfbb958518a1882c3b312f4d8e4
FranckRICHARD01 <franckrichard033@gmail.com>
2025-04-07 23:51:19 +0200

    Warning licence corrected...

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage(
-    "⚠️  RbowtieCuda is distributed under the BSD 3-Clause License.\n",
+    "RbowtieCuda is distributed under the BSD 3-Clause License.\n",
     "Please read the LICENSE file carefully before use.\n\n",
     "Note:\n",
     "- Redistribution and use (source/binary, with/without modification) are permitted\n",
